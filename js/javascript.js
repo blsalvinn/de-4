e8147d69279240944f10bb47b8379d07d370ad63
blsalvinn <60783447+blsalvinn@users.noreply.github.com>
2021-09-13 08:39:31 +0700

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,3 @@
-// $(document).ready(function() {
-//     $('.contracts-small-mobile').click(function() {
-//         $('.nav-menu').toggleClass('show');
-//     })
-// });
 jQuery(document).ready(function() {
     $('.menu-toggle').click(function(e) {
         $('.nav-menu').toggleClass('active');
@@ -14,38 +9,56 @@ jQuery(document).ready(function() {
         e.preventDefault();
     });
     // client-say
-    let slidePosition = 0;
-    const slides = $('.carousel-item');
-    const dots = $('.dots');
-    const totalDot = dots.length;
-    $('#test').on('click', function() {
-        // console.log(totalDot);
-    });
-    $('#dot-event').on('click', function() {
-        moveDotSilde();
-    });
+    var stt = 0;
+    var endImg = $(".slide:last").attr("idx");
+    var changeImg = function(stt) {
+        $(".slide").hide();
+        $(".slide").eq(stt).show();
+        $(".btn-client button").removeClass("active");
+        $(".btn-client button").eq(stt).addClass("active");
 
-    function updateSlidePosition() {
-        for (let slide of slides) {
-            slide.classList.remove('active');
-            slide.classList.add('carousel-item--hidden');
-        }
-        for (let dot of dots) {
-            dot.classList.remove('active');
-            dot.classList.add('dots-opacity');
-        }
-        slides[slidePosition].classList.add('active');
-        dots[slidePosition].classList.add('active');
-    }
+        // clearInterval(interval);
+        // timer();
+    };
 
-    function moveDotSilde() {
-        if (slidePosition === totalDot - 1) {
-            slidePosition = 0;
-        } else {
-            slidePosition++;
-        }
-        updateSlidePosition()
-    }
+    $(".btn-client button").click(function() {
+        stt = $(this).attr("idx");
+
+        changeImg(stt);
+    });
+    // let slidePosition = 0;
+    // const slides = $('.carousel-item');
+    // const dots = $('.dots');
+    // const totalDot = dots.length;
+
+    // function currentSlide(dots_event) {
+    //     alert(dots_event);
+    // }
+    // $('#dot-event').on('click', function() {
+    //     moveDotSilde();
+    // });
+
+    // function updateSlidePosition() {
+    //     for (let slide of slides) {
+    //         slide.classList.remove('active');
+    //         slide.classList.add('carousel-item--hidden');
+    //     }
+    //     for (let dot of dots) {
+    //         dot.classList.remove('active');
+    //         dot.classList.add('dots-opacity');
+    //     }
+    //     slides[slidePosition].classList.add('active');
+    //     dots[slidePosition].classList.add('active');
+    // }
+
+    // function moveDotSilde() {
+    //     if (slidePosition === totalDot - 1) {
+    //         slidePosition = 0;
+    //     } else {
+    //         slidePosition++;
+    //     }
+    //     updateSlidePosition()
+    // }
     // brand
     let slidePosition_brand = 0;
     const slides_brand = $('.mySlides');
@@ -82,43 +95,4 @@ jQuery(document).ready(function() {
         }
         updateSlidePosition_brand()
     }
-    // var slideIndex = 1;
-    // showSlides(slideIndex);
-
-    // function plusSlidess(n) {
-    //     showSlides(slideIndex += n);
-    // }
-
-    // function showSlides(n) {
-    //     var i;
-    //     var slides_brand = $('.mySlides');
-    //     if (n > slides_brand.length) {
-    //         slideIndex = 1
-    //     }
-    //     if (n < 1) {
-    //         slideIndex = slides_brand.length
-    //     }
-    //     for (i = 0; i < slides_brand.length; i++) {
-    //         slides_brand[i].style.display = "none";
-    //     }
-    //     slides_brand[slideIndex - 1].style.display = "block";
-    // }
-    // var slideIndex = 1;
-    // showDivs(slideIndex);
-
-    // function plusDivs(n) {
-    //     showDivs(slideIndex += n);
-    // }
-
-    // function showDivs(n) {
-    //     var i;
-    //     var x = $('.items-brand');
-    //     if (n > x.length) { slideIndex = 1 }
-    //     if (n < 1) { slideIndex = x.length }
-    //     for (i = 0; i < x.length; i++) {
-    //         x[i].style.display = "none";
-    //     }
-    //     x[slideIndex - 1].style.display = "block";
-    // }
-
 });
