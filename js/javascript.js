de716c1c58f86a5493d78cbe8efe839982766bb3
blsalvinn <60783447+blsalvinn@users.noreply.github.com>
2021-09-14 08:25:08 +0700

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -26,73 +26,40 @@ jQuery(document).ready(function() {
 
         changeImg(stt);
     });
-    // let slidePosition = 0;
-    // const slides = $('.carousel-item');
-    // const dots = $('.dots');
-    // const totalDot = dots.length;
-
-    // function currentSlide(dots_event) {
-    //     alert(dots_event);
-    // }
-    // $('#dot-event').on('click', function() {
-    //     moveDotSilde();
+    // brand
+    // let slidePosition_brand = 0;
+    // const slides_brand = $('.mySlides');
+    // const totalSlides_brand = slides_brand.length;
+    // $('#prev').bind('click', function() {
+    //     movetoPrevSlide();
+    // });
+    // $('#next').bind('click', function() {
+    //     movetoNextSlide();
     // });
 
-    // function updateSlidePosition() {
-    //     for (let slide of slides) {
-    //         slide.classList.remove('active');
-    //         slide.classList.add('carousel-item--hidden');
+    // function updateSlidePosition_brand() {
+    //     for (let slide_brand of slides_brand) {
+    //         slide_brand.classList.remove('active');
+    //         slide_brand.classList.add('mySlides--hidden');
     //     }
-    //     for (let dot of dots) {
-    //         dot.classList.remove('active');
-    //         dot.classList.add('dots-opacity');
-    //     }
-    //     slides[slidePosition].classList.add('active');
-    //     dots[slidePosition].classList.add('active');
+    //     slides_brand[slidePosition_brand].classList.add('active');
     // }
 
-    // function moveDotSilde() {
-    //     if (slidePosition === totalDot - 1) {
-    //         slidePosition = 0;
+    // function movetoNextSlide() {
+    //     if (slidePosition_brand === totalSlides_brand - 1) {
+    //         slidePosition_brand = 0;
     //     } else {
-    //         slidePosition++;
+    //         slidePosition_brand++;
     //     }
-    //     updateSlidePosition()
+    //     updateSlidePosition_brand()
     // }
-    // brand
-    let slidePosition_brand = 0;
-    const slides_brand = $('.mySlides');
-    const totalSlides_brand = slides_brand.length;
-    $('#prev').bind('click', function() {
-        movetoPrevSlide();
-    });
-    $('#next').bind('click', function() {
-        movetoNextSlide();
-    });
 
-    function updateSlidePosition_brand() {
-        for (let slide_brand of slides_brand) {
-            slide_brand.classList.remove('active');
-            slide_brand.classList.add('mySlides--hidden');
-        }
-        slides_brand[slidePosition_brand].classList.add('active');
-    }
-
-    function movetoNextSlide() {
-        if (slidePosition_brand === totalSlides_brand - 1) {
-            slidePosition_brand = 0;
-        } else {
-            slidePosition_brand++;
-        }
-        updateSlidePosition_brand()
-    }
-
-    function movetoPrevSlide() {
-        if (slidePosition_brand === 0) {
-            slidePosition_brand = totalSlides_brand - 1;
-        } else {
-            slidePosition_brand--;
-        }
-        updateSlidePosition_brand()
-    }
+    // function movetoPrevSlide() {
+    //     if (slidePosition_brand === 0) {
+    //         slidePosition_brand = totalSlides_brand - 1;
+    //     } else {
+    //         slidePosition_brand--;
+    //     }
+    //     updateSlidePosition_brand()
+    // }
 });
